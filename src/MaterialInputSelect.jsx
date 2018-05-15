c02cbd6fdda0d066db761633785f7b209916f1ad
Charles Burgess <consulting@cvburgess.com>
2018-05-15 17:45:21 -0400

    add support for multiSelect and non-native selects

--- src/MaterialInputSelect.jsx
+++ src/MaterialInputSelect.jsx
@@ -1,33 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { TextField } from "material-ui";
+import { Checkbox, MenuItem, TextField } from "material-ui";
 
 // See https://github.com/mui-org/material-ui/issues/10938 for why haxx
+// If using in an MUI modal/dialog, be sure to pass `disableEnforceFocus=true`
+
+const defaultValue = multiSelect => (multiSelect ? [] : "");
+const getLabelForValues = values => {
+  if (!values || !values.length) return "Select";
+  return values.length > 1 ? `${values.length} Selected` : values[0];
+};
 
 const MaterialInputSelect = ({
   field,
-  form: { errors, touched },
+  form: { errors, handleBlur, touched },
   label,
+  multiSelect,
   options,
   ...props
-}) => (
-  <TextField
-    {...props}
-    {...field}
-    value={field.value !== undefined ? field.value : ""}
-    error={Boolean(touched[field.name] && errors[field.name])}
-    label={(touched[field.name] && errors[field.name]) || label}
-    select
-    SelectProps={{ native: true }}
-  >
-    {field.value === "" && <option value="">Select</option>}
-    {options.map(option => (
-      <option key={option.value} value={option.value}>
-        {option.label}
-      </option>
-    ))}
-  </TextField>
-);
+}) => {
+  const SelectProps = multiSelect
+    ? {
+        multiple: true,
+        renderValue: () => getLabelForValues(field.value)
+      }
+    : undefined;
+  return (
+    <TextField
+      {...props}
+      {...field}
+      value={
+        field.value !== undefined ? field.value : defaultValue(multiSelect)
+      }
+      error={Boolean(touched[field.name] && errors[field.name])}
+      label={(touched[field.name] && errors[field.name]) || label}
+      onBlur={event => {
+        event.target.name = field.name; // eslint-disable-line
+        handleBlur(event);
+      }}
+      select
+      SelectProps={SelectProps}
+    >
+      {field.value === "" && <MenuItem value="">Select</MenuItem>}
+      {options.map(option => (
+        <MenuItem value={option.value} key={option.value}>
+          {multiSelect && (
+            <Checkbox
+              color="primary"
+              checked={field.value ? field.value.includes(option.value) : false}
+            />
+          )}
+          {option.label}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
 
 MaterialInputSelect.propTypes = {
   field: PropTypes.shape({
@@ -42,6 +70,7 @@ MaterialInputSelect.propTypes = {
     // the rest of the formik bag too
   }).isRequired,
   label: PropTypes.string.isRequired,
+  multiSelect: PropTypes.bool,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.any,
@@ -51,6 +80,7 @@ MaterialInputSelect.propTypes = {
 };
 
 MaterialInputSelect.defaultProps = {
+  multiSelect: false,
   options: []
 };
 
